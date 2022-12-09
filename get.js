869fcb88e1e34a4ee0c92ef5bd6ff4cec36453e9
poojastudent <poojabishu96@gmail.com>
2022-12-09 15:15:27 +0530

    new

--- get.js
+++ get.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 2000;
 app.get('/', (req, res) => {
     res.send("data for reading")
 })
 app.listen(port, () => {
-    console.log('listening on port http://localhost:3000')
+    console.log('listening on port http://localhost2000')
 })
