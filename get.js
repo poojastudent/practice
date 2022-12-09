4bf6a4c9cde405f8c8a39cd032fa9bcffb941015
poojastudent <poojabishu96@gmail.com>
2022-12-09 13:56:01 +0530

    get method

--- get.js
+++ get.js
@@ -1,24 +1,9 @@
-
-const rp = require("request-promise");
-const express = require("express");
+const express = require('express');
 const app = express();
-const body_parser = require("body-parser");
-const fs = require("fs");
-const { readFile } = require("fs/promises");
 const port = 3000;
-app.use(body_parser.json());
-async function main() {
-    rp({
-        url: 'http://localhost3000',
-        json: true,
-        method: 'get',
-
-    })
-    app.listen(port, () => {
-        console.log("server listening on " + port);
-    });
-    app.get('/', (req, res) => {
-        var data = fs.readFileSync("data.json", 'utf-8')
-        res.send(data);
-    })
-}
+app.get('/', (req, res) => {
+    res.send("data for reading")
+})
+app.listen(port, () => {
+    console.log('listening on port http://localhost3000')
+})
